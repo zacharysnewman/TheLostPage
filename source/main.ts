fd254a3cc0081fe50464a7f8729f5386a54c5d9e
Zack Newman <zacknewman@comcast.net>
2020-08-22 22:25:48 -0700

    Init Commit

---
+++ source/main.ts
@@ -0,0 +1,24 @@
+// 9 x 9 map
+
+// Idle mechanics
+/*
+Primary Goal:
+- Reach Level 404 and save the missing page
+
+Secondary Goals:
+- Defeat Enemies to unlock the next levels
+
+Actions:
+- Send Knights on Quest
+
+Upgrades:
+- Speed
+- Number of knights
+- Level Up (Health/Damage)
+- Automatic Questing for # Knights
+- Automatic Elevator
+*/
+
+describe("Level Display", () => {
+  test("");
+});
